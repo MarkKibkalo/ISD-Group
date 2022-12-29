bfde11065ce5a762b3f67c4886b9aa9a10d282f9
MarkKibkalo <krono1337@gmail.com>
2022-12-29 18:11:12 +0200

    fix govno_kod

--- script/video-content.js
+++ script/video-content.js
@@ -3,14 +3,13 @@ let projectContent = document.querySelectorAll('[data-project]');
 let selectedProject = null;
 let video = null;
 let hidableElemetns = document.querySelectorAll(".project__item-link");
-let prevProject = null;
 
 projectContent.forEach(function (item) {
    item.addEventListener('mouseover', function (obj) {
       if(video !== null) video.classList.add('hidden')
       video = document.querySelector('#' + obj.target.attributes['data-project'].value);
       video.currentTime = 0;
-      video.classList.remove('hidden')
+      video.classList.remove('hidden');
       let el = null;
       for(el in hidableElemetns) hidableElemetns.item(el).setAttribute('data-hidable', 'true');
       obj.target.setAttribute('data-hidable', 'false');
